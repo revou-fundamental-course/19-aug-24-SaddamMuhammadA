265300b61d421b8a72528dc93aac7274d13e86f4
Saddam Muhammad A <gsaddam855@gmail.com>
2024-08-30 02:21:21 +0700

    Menambahkan deskripsi

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+//Toggle & Resposive Navigation
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const navList = document.querySelector("nav");
+
+  burger.addEventListener("click", () => {
+    navList.classList.toggle("nav-active");
+    burger.classList.toggle("toggle-burger");
+  });
+};
+
+navSlide();
+
 document
   .getElementById("contact-form")
   .addEventListener("submit", function (event) {
